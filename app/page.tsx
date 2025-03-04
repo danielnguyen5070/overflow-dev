b94c77fbba687965482286cbe05228d21f1ed14c
Daniel <viet.dan.1613@gmail.com>
2025-03-04 17:08:23 +0700

    feature: assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>
-        <h1 className="background-light700_dark300">
-          Create Next App Create Create Create Create items-center
-          justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 fon
-        </h1>
-      </div>
+    <div className="">
+      <p className="font-space-grotesk">dummy text here</p>
+      <p className="font-inter">dummy text here</p>
     </div>
   );
 }
