b94f6e7df30b4385bd03bd07172589ac8c802dc4
Daniel <viet.dan.1613@gmail.com>
2025-03-18 13:51:46 +0700

    feature: ui for sign-in and sign-up

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="">
-      <p className="font-space-grotesk">dummy text here</p>
-      <p className="font-inter">dummy text here</p>
-    </div>
-  );
-}
