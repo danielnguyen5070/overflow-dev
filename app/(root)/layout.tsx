47cfcce352286383d04f96497353c7f7887c8b8e
Daniel <viet.dan.1613@gmail.com>
2025-03-19 17:06:58 +0700

    feature: left side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,22 @@
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 import React from "react";
 
 const LayoutRoot = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+
+        <RightSidebar />
+      </div>
     </main>
   );
 };
