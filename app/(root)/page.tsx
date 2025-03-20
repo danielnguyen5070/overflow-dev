18db355b569c31cf5d0a37107fbb92cd2051881c
Daniel <viet.dan.1613@gmail.com>
2025-03-20 16:10:01 +0700

    Feature: home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,34 @@
+import HomeFilter from "@/components/search/HomeFilter";
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import { Link } from "lucide-react";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      <HomeFilter />
+    </>
+  );
 };
 
 export default Home;
